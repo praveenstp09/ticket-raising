7cf35e14c0c6df646d0a1c1fd733a68dbf681f20
Praveen Verma <praveenstp2003@gmail.com>
2024-11-24 18:32:09 +0530

    adding filter-Search

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,68 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
+import Card from "./Component/Card";
 
 function App() {
   const [formDataRaising, setFormDataRaising] = useState({
     title: "",
     description: "",
     createdBy: "",
-    priority: "",
+    priority: "low",
   });
   async function handleFormRaising(e) {
     e.preventDefault();
+    // console.log(formDataRaising);
     const response = await fetch("http://localhost:8000/generateTicket", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(formData),
+      body: JSON.stringify(formDataRaising),
     });
-    const data = response.json();
+    const data =await response.json();
     console.log(data);
+
   }
-  
+
   const handleChangeRaising = (e) => {
     const name = e.target.name;
     const value = e.target.value;
     console.log(name + " " + value);
     setFormDataRaising({
-      ...formData,
+      ...formDataRaising,
       [name]: value,
     });
   };
 
-  const [searchStatus,setSearchStatus]=useState("all")
-  const [priority,setPriority]=useState("all")
-  async function handleFormFilter(e){
-    
+  const [filterDate, setFilterData] = useState({
+    filterStatus: "all",
+    filterPriority: "all",
+  });
+
+  const handleChangeFilter = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(name + " " + value);
+    setFilterData({
+      ...formDataRaising,
+      [name]: value,
+    });
+  };
+
+  const [ticketsList,setTicketsList]=useState([])
+
+  useEffect(() => {
+    getTicketList();
+  }, [filterDate]);
+
+  async function getTicketList(){
+    const resp=await fetch('http://localhost');
+    const data=resp.json();
+    console.log(data);
+    setTicketsList(data);
   }
+
   return (
     <>
       <div className="riasing-platform">
@@ -75,7 +101,9 @@ function App() {
             value={formDataRaising.priority}
             onChange={handleChangeRaising}
           >
-            <option value="low">Low</option>
+            <option value="low" selected>
+              Low
+            </option>
             <option value="medium">Medium</option>
             <option value="high">High</option>
           </select>
@@ -84,35 +112,54 @@ function App() {
         </form>
       </div>
 
-      <div className="filter-search">
-        <h2>Filters and search</h2>
-        <form onSubmit={handleFormFilter}>
-        <label htmlFor="status">status : </label>
-        <select
+      <div>
+        <div className="filter-search">
+          <h2>Filters and search</h2>
+
+          <label htmlFor="status">status : </label>
+          <select
             name="status"
-            value={searchStatus}
-            onChange={(e)=>setSearchStatus(e.target.value)}
+            value={filterDate.filterStatus}
+            onChange={handleChangeFilter}
           >
-            <option value="all">all</option>
+            <option value="all" selected>
+              all
+            </option>
             <option value="solved">Solved</option>
             <option value="unsolved">Unsolved</option>
-            
           </select>
           <br />
 
           <label htmlFor="priority">priority : </label>
           <select
             name="priority"
-            value={priority}
-            onChange={(e)=>setPriority(e.target.value)}
+            value={filterDate.filterPriority}
+            onChange={handleChangeFilter}
           >
-            <option value="all">All</option>
+            <option value="all" selected>
+              All
+            </option>
             <option value="low">Low</option>
             <option value="medium">Medium</option>
             <option value="high">High</option>
           </select>
           <br />
-        </form>
+        </div>
+
+        <h2>Tickets</h2>
+        <div className="card" style={{display:"flex", alignItems:"center",justifyContent:"flex-start"}}>
+
+          {ticketsList.map((ticket)=>{
+            return (
+              <div style={{border:"2px solid black", padding:"5px"}}>
+                <p>{ticket.description}</p>
+                <p>Created by:{ticket.createdBy}</p>
+                <p>Priority : {ticket.priority}</p>
+                <button>Delete</button>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </>
   );
